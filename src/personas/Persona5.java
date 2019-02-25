9a4ddfb1aeca6696c97cfc1ab905d8980a081b8e
mayurimamtani <mayuri_mamtani@hotmail.com>
2019-02-25 18:24:41 -0500

    Create Persona5.java

---
+++ src/personas/Persona5.java
@@ -0,0 +1,16 @@
+
+package personas;
+
+import java.util.List;
+import schemas.Film;
+import schemas.User;
+
+public class Persona5 extends Persona {
+    public static User createUser() {
+        return null;
+    }
+
+    public static List<Film> createMovies() {
+        return null;
+    }
+}
