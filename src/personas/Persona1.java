d6c1752e1af0793d0af58a043691ec28e92c8e0b
mayurimamtani <mayuri_mamtani@hotmail.com>
2019-02-27 14:04:44 -0500

    Update Persona1.java

--- src/personas/Persona1.java
+++ src/personas/Persona1.java
@@ -3,6 +3,7 @@ package personas;
 // Persona 1 is focused on the high-level aspects of movie selection. 
 
 // Persona 1 cares about MOOD and GENRE of a potential movie. 
+
 // Persona 1 does not care about low-level aspects such as RATING and PLOT SUMMARY. 
 
 // Persona 1 is typically in a "happy" mood and enjoys the genres "comedy" and "drama".
