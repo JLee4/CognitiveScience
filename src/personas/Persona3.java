746e2e9937354076d52c9bf942ab605ba1a252fc
mayurimamtani <mayuri_mamtani@hotmail.com>
2019-02-27 13:38:07 -0500

    Update Persona3.java

--- src/personas/Persona3.java
+++ src/personas/Persona3.java
@@ -1,12 +1,27 @@
 package personas;
 
+// Persona 3 does not watch many movies a lot, when he/she does, he/she prioritizes the RATING and PLOT SUMMARY of the movie. 
+
+// Persona 3 cares about their MOOD and enjoys all movie GENRES except for "drama" and "thriller"
+
+// Persona 3 is typically in a "sad" mood and will only watch movies if the cast members are well known. 
+
+// Persona 3 will only watch movies alone. 
+
 import java.util.List;
 import schemas.Film;
 import schemas.User;
 
 public class Persona3 extends Persona {
+
     public static User createUser() {
-        return null;
+        List<String> preferredGenres = new ArrayList<String>();
+        preferredGenres.add("COMEDY");
+        preferredGenres.add("ROMCOM");
+        preferredGenres.add("ACTION");
+        preferredGenres.add("HORROR");
+  
+        User persona3 = new User(Mood.SAD, FreeTime.THREE_HOURS, GroupSize.ALONE, Attention.MEDIUM, preferredGenres);
     }
 
     public static List<Film> createMovies() {
